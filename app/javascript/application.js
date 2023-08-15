49b9326e9c789a5a6d081b1dcd09f3cb9743366c
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-08-15 11:05:04 -0500

    Checked linters

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,15 +10,22 @@ import Greeting from "./components/Greeting";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+function App () {
+  return (
+    <div>
+      <h1>React App</h1>
+    </div>
+  )
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
     <Provider store={store}>
+      <App />
       <Greeting />
     </Provider>
     </Router>
   </React.StrictMode>
 );
-
-/* ReactDOM.render(<Greeting />, document.getElementById("root")); */
