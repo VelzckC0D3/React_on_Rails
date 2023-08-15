88660911aec48480ee169442b104b28daf1c193b
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-08-15 10:54:22 -0500

    Add router

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,10 +4,7 @@ import "./controllers";
 import React from "react";
 import ReactDOM from "react-dom";
 import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  BrowserRouter,
+  BrowserRouter as Router
 } from "react-router-dom";
 import Greeting from "./components/Greeting";
 import { Provider } from "react-redux";
@@ -16,9 +13,11 @@ import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Router>
     <Provider store={store}>
       <Greeting />
     </Provider>
+    </Router>
   </React.StrictMode>
 );
 
